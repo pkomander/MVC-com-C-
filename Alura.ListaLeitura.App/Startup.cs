3ae4c2b1ff898d00068e63e2c914089f468f36cf
Paulo Ricardo Silva de Araújo <54222357+pkomander@users.noreply.github.com>
2021-02-20 13:44:03 -0300

    primeira parte do curso sobre mvc com c#, onde o foco e criar uma webapp com padrão MVC.

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    internal class Startup
+    {
+        // applicationBuilder constroi o pipiline de exibição para aplicação
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(LivrosParaLer);
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+            // quando a request for chamanda sera mandada a resposta no contex.response
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+            
+        }
+    }
+}
